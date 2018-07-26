271ebcdff832ce2eb495875dcf74973b83dbb300
Tetsuya Idota <tidota@openrobotics.org>
2018-07-26 13:53:33 -0700

    Add odometry for converter

--- subt_example/scripts/cmdvel_transform.py
+++ subt_example/scripts/cmdvel_transform.py
@@ -6,30 +6,41 @@ import rospy
 from geometry_msgs.msg import (Twist, Pose, Point, Quaternion, PoseStamped)
 import tf.transformations as tf
 import numpy
+from threading import Lock
 
-def callbackCmdVel(data):
-	vel = data
-	newPose = currentPose
-	quaternion = (newPose.orientation.x, newPose.orientation.y, newPose.orientation.z, newPose.orientation.w)
-	vec = (vel.linear.x, vel.linear.y, vel.linear.z, 1)
-	Rot = tf.quaternion_matrix(quaternion).tolist()
-	buff = numpy.matmul(Rot, vec)
-	newPose.position.x += buff[0]
-	newPose.position.y += buff[1]
-	newPose.position.z += buff[2]
-	quaternion0 = (newPose.orientation.x, newPose.orientation.y, newPose.orientation.z, newPose.orientation.w)
-	#print(quaternion0)
-	quaternion1 = tf.quaternion_from_euler(vel.angular.x, vel.angular.y, vel.angular.z)
-	#print(quaternion1)
-	quaternion = tf.quaternion_multiply(quaternion1, quaternion0)
-	#print(quaternion)
-	newPose.orientation.x = quaternion[0]
-	newPose.orientation.y = quaternion[1]
-	newPose.orientation.z = quaternion[2]
-	newPose.orientation.w = quaternion[3]
-	poseStamped = PoseStamped()
-	poseStamped.pose = newPose
-	pub.publish(poseStamped)
+class Converter:
+	def __init__(self):
+		self.currentPose = None
+		self.pub = None
+		self.mutex = Lock()
+
+	def callbackOdom(self, data):
+		self.mutex.acquire()
+		self.currentPose = data
+		self.mutex.release()
+
+	def callbackCmdVel(self, data):
+		vel = data
+		self.mutex.acquire()
+		newPose = self.currentPose
+		self.mutex.release()
+		quaternion = (newPose.orientation.x, newPose.orientation.y, newPose.orientation.z, newPose.orientation.w)
+		vec = (vel.linear.x, vel.linear.y, vel.linear.z, 1)
+		Rot = tf.quaternion_matrix(quaternion).tolist()
+		buff = numpy.matmul(Rot, vec)
+		newPose.position.x += buff[0]
+		newPose.position.y += buff[1]
+		newPose.position.z += buff[2]
+		quaternion0 = (newPose.orientation.x, newPose.orientation.y, newPose.orientation.z, newPose.orientation.w)
+		quaternion1 = tf.quaternion_from_euler(vel.angular.x, vel.angular.y, vel.angular.z)
+		quaternion = tf.quaternion_multiply(quaternion1, quaternion0)
+		newPose.orientation.x = quaternion[0]
+		newPose.orientation.y = quaternion[1]
+		newPose.orientation.z = quaternion[2]
+		newPose.orientation.w = quaternion[3]
+		poseStamped = PoseStamped()
+		poseStamped.pose = newPose
+		self.pub.publish(poseStamped)
 
 if __name__=="__main__":
 	filtered_argv = rospy.myargv(sys.argv)
@@ -39,16 +50,9 @@ if __name__=="__main__":
 
 	rospy.init_node('cmd_vel_converter', anonymous=True)
 
-	rospy.Subscriber(filtered_argv[1], Twist, callbackCmdVel)
-	pub = rospy.Publisher(filtered_argv[2], PoseStamped, queue_size=1)
-	buff = eval(filtered_argv[3])
-	currentPose = Pose()
-	currentPose.position.x = buff[0]
-	currentPose.position.y = buff[1]
-	currentPose.position.z = buff[2]
-	currentPose.orientation.x = buff[3]
-	currentPose.orientation.y = buff[4]
-	currentPose.orientation.z = buff[5]
-	currentPose.orientation.w = buff[6]
+	converter = Converter()
+	rospy.Subscriber(filtered_argv[1], Pose, converter.callbackOdom)
+	rospy.Subscriber(filtered_argv[2], Twist, converter.callbackCmdVel)
+	converter.pub = rospy.Publisher(filtered_argv[3], PoseStamped, queue_size=1)
 
 	rospy.spin()
