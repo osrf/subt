77783620c8d27a2a0a9ec485ccb9da71a6e94a6a
Tetsuya Idota <tidota@openrobotics.org>
2018-07-25 18:35:54 -0700

    Make it to run

--- subt_example/scripts/cmdvel_transform.py
+++ subt_example/scripts/cmdvel_transform.py
@@ -4,7 +4,7 @@ import sys
 import rospy
 
 from geometry_msgs.msg import (Twist, Pose, Point, Quaternion, PoseStamped)
-import tf
+import tf.transformations as tf
 
 currentPose=Pose(Point(0,0,0),Quaternion(0,0,0,0))
 pub=""
@@ -15,9 +15,15 @@ def callbackPose(data):
 def callbackCmdVel(data):
 	vel = data
 	newPose = currentPose
-	newPose.position += vel.linear
-	q = Quaternion(vel.angular.x, vel.angular.y, vel.angular.z)
-	newPose.orientation *= q
+	newPose.position.x += vel.linear.x
+	newPose.position.y += vel.linear.y
+	newPose.position.z += vel.linear.z
+	quaternion = (newPose.orientation.x, newPose.orientation.y, newPose.orientation.z, newPose.orientation.w)
+	quaternion = tf.quaternion_multiply(quaternion, tf.quaternion_from_euler(vel.angular.x, vel.angular.y, vel.angular.z))
+	newPose.orientation.x = quaternion[0]
+	newPose.orientation.y = quaternion[1]
+	newPose.orientation.z = quaternion[2]
+	newPose.orientation.w = quaternion[3]
 	poseStamped = PoseStamped()
 	poseStamped.pose = newPose
 	pub.publish(poseStamped)
@@ -28,6 +34,6 @@ if __name__=="__main__" and len(sys.argv) == 4:
 
 	rospy.Subscriber(sys.argv[1] + "/pose", Pose, callbackPose)
 	rospy.Subscriber(sys.argv[2] + "/cmd_vel", Twist, callbackCmdVel)
-	pub = rospy.Publisher(sys.argv[3] + '/pose', PoseStamped, queue_size = 1)
+	pub = rospy.Publisher(sys.argv[3] + '/pose', PoseStamped)
 
 	rospy.spin()
