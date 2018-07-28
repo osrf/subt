c3dc6eaf55e91fb5b434fbb6565d741d8b2174cb
Tetsuya Idota <tidota@openrobotics.org>
2018-07-27 19:00:52 -0700

    Fix joy stick control

--- subt_example/src/teleop_node.cc
+++ subt_example/src/teleop_node.cc
@@ -40,8 +40,14 @@ class SubtTeleop
   /// \brief Scale value for the linear axis input.
   private: double linearScale;
 
+  /// \brief Scale value for the linear axis input (turbo mode).
+  private: double linearScaleTurbo;
+
   /// \brief Scale value for the angular axis input.
   private: double angularScale;
+
+  /// \brief Scale value for the angular axis input (turbo mode).
+  private: double angularScaleTurbo;
 
   /// \brief Index for the vertical axis (z axis) of the joy stick.
   private: int vertical;
@@ -52,14 +58,26 @@ class SubtTeleop
   /// \brief Scale value for the vertical axis (z axis) input.
   private: double verticalScale;
 
+  /// \brief Scale value for the vertical axis (z axis) input (turbo mode).
+  private: double verticalScaleTurbo;
+
   /// \brief Scale value for the horizontal axis (y axis) input.
   private: double horizontalScale;
 
+  /// \brief Scale value for the horizontal axis (y axis) input (turbo mode).
+  private: double horizontalScaleTurbo;
+
   /// \brief Index for the button to enable the joy stick control.
-  private: int enableTrigger;
+  private: int enableButton;
 
   /// \brief Index for the button to enable the joy stick control (Turbo mode).
-  private: int enableTurboTrigger;
+  private: int enableTurboButton;
+
+  /// \brief Index for the trigger to turn lights on.
+  private: int lightOnTrigger;
+
+  /// \brief Index for the trigger to turn lights off.
+  private: int lightOffTrigger;
 
   /// \brief Subscriber to get input values from the joy control.
   private: ros::Subscriber joySub;
@@ -87,24 +105,40 @@ class SubtTeleop
 
 /////////////////////////////////////////////////
 SubtTeleop::SubtTeleop():
-  linear(1), angular(0), linearScale(0), angularScale(0),
-  vertical(4), horizontal(3), verticalScale(0), horizontalScale(0),
-  enableTrigger(9), enableTurboTrigger(10)
+  linear(1), angular(0), linearScale(0), linearScaleTurbo(0), angularScale(0),
+  angularScaleTurbo(0), vertical(4), horizontal(3), verticalScale(0),
+  verticalScaleTurbo(0), horizontalScale(0), horizontalScaleTurbo(0),
+  enableButton(4), enableTurboButton(5), lightOnTrigger(2), lightOffTrigger(5)
 {
   // Load joy control settings. Setting values must be loaded by rosparam.
   this->nh.param("axis_linear", this->linear, this->linear);
   this->nh.param("axis_angular", this->angular, this->angular);
   this->nh.param("scale_linear", this->linearScale, this->linearScale);
+  this->nh.param(
+    "scale_linear_turbo", this->linearScaleTurbo, this->linearScaleTurbo);
   this->nh.param("scale_angular", this->angularScale, this->angularScale);
+  this->nh.param(
+    "scale_angular_turbo", this->angularScaleTurbo, this->angularScaleTurbo);
 
   this->nh.param("axis_vertical", this->vertical, this->vertical);
   this->nh.param("axis_horizontal", this->horizontal, this->horizontal);
   this->nh.param("scale_vertical", this->verticalScale, this->verticalScale);
   this->nh.param(
+    "scale_vertical_turbo", this->verticalScaleTurbo, this->verticalScaleTurbo);
+  this->nh.param(
     "scale_horizontal", this->horizontalScale, this->horizontalScale);
+  this->nh.param(
+    "scale_horizontal_turbo",
+    this->horizontalScaleTurbo, this->horizontalScaleTurbo);
 
-  this->nh.param("enable_trigger", this->enableTrigger, this->enableTrigger);
-  this->nh.param("enable_turbo_trigger", this->enableTurboTrigger, -1);
+  this->nh.param("enable_button", this->enableButton, this->enableButton);
+  this->nh.param(
+    "enable_turbo_button", this->enableTurboButton, this->enableTurboButton);
+
+  this->nh.param(
+    "light_on_trigger", this->lightOnTrigger, this->lightOnTrigger);
+  this->nh.param(
+    "light_off_trigger", this->lightOffTrigger, this->lightOffTrigger);
 
   this->nh.getParam("button_map", this->joyButtonIndexMap);
 
@@ -119,7 +153,8 @@ SubtTeleop::SubtTeleop():
     // Create a publisher object to generate a velocity command, and associate
     // it to the corresponding robot's name.
     this->velPubMap[robotName]
-      = this->nh.advertise<geometry_msgs::Twist>(robotName + "/cmd_vel", 1);
+      = this->nh.advertise<geometry_msgs::Twist>(
+        robotName + "/cmd_vel", 1, true);
 
     // Create service clients to control flashlights/LEDs, and associate them
     // to the corresponding service names.
@@ -138,15 +173,15 @@ SubtTeleop::SubtTeleop():
   // Subscribe "joy" topic to listen to the joy control.
   this->joySub
     = this->nh.subscribe<sensor_msgs::Joy>(
-      "joy", 10, &SubtTeleop::joyCallback, this);
+      "joy", 1, &SubtTeleop::joyCallback, this);
 }
 
 /////////////////////////////////////////////////
 void SubtTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
   std_srvs::SetBool srv;
-  // If L button was pressed, turn the lights on.
-  if (joy->buttons[this->joyButtonIndexMap["L"]])
+  // If LT was triggered, turn the lights on.
+  if (joy->axes[this->lightOnTrigger] < 0)
   {
     srv.request.data = true;
     for (auto suffix: this->lightSrvSuffixList)
@@ -156,8 +191,8 @@ void SubtTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     }
     return;
   }
-  // If R button was pressed, turn the lights off.
-  if (joy->buttons[this->joyButtonIndexMap["R"]])
+  // If RT was triggered, turn the lights off.
+  if (joy->axes[this->lightOffTrigger] < 0)
   {
     srv.request.data = false;
     for (auto suffix: this->lightSrvSuffixList)
@@ -174,27 +209,37 @@ void SubtTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     if (joy->buttons[this->joyButtonIndexMap[pair.first]])
     {
       this->currentRobot = pair.second;
+      return;
     }
   }
 
   geometry_msgs::Twist twist;
-  // Since a trigger value spans from 1 to -1, it is remapped so it does from 0
-  // to 1.
-  double triggerRate = -0.5 * joy->axes[this->enableTrigger] + 0.5;
-  if (triggerRate == 0)
-    triggerRate = -0.5 * joy->axes[this->enableTurboTrigger] + 0.5;
 
   // If the trigger values are non zero, calculate control values.
-  if (triggerRate > 0)
+  if (joy->buttons[this->enableButton])
   {
     twist.linear.x
-      = this->linearScale * joy->axes[this->linear] * triggerRate;
+      = this->linearScale * joy->axes[this->linear];
     twist.linear.y
-      = this->horizontalScale * joy->axes[this->horizontal] * triggerRate;
+      = this->horizontalScale * joy->axes[this->horizontal];
     twist.linear.z
-      = this->verticalScale * joy->axes[this->vertical] * triggerRate;
+      = this->verticalScale * joy->axes[this->vertical];
     twist.angular.z
-      = this->angularScale * joy->axes[this->angular] * triggerRate;
+      = this->angularScale * joy->axes[this->angular];
+
+    // Publish the control values.
+    this->velPubMap[this->currentRobot].publish(twist);
+  }
+  else if (joy->buttons[this->enableTurboButton])
+  {
+    twist.linear.x
+      = this->linearScaleTurbo * joy->axes[this->linear];
+    twist.linear.y
+      = this->horizontalScaleTurbo * joy->axes[this->horizontal];
+    twist.linear.z
+      = this->verticalScaleTurbo * joy->axes[this->vertical];
+    twist.angular.z
+      = this->angularScaleTurbo * joy->axes[this->angular];
 
     // Publish the control values.
     this->velPubMap[this->currentRobot].publish(twist);
