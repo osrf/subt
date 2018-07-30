45cbfdad6292e1fec76a0113f48519c473bc48f2
Tetsuya Idota <tidota@openrobotics.org>
2018-07-30 12:14:06 -0700

    Rename led test to flashlight test

--- subt_gazebo/test/flashlight.cc
+++ subt_gazebo/test/flashlight.cc
@@ -20,62 +20,45 @@
 #include <thread>
 #include <vector>
 
+#include <gazebo/gazebo.hh>
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/rendering/Camera.hh>
+#include <gazebo/transport/transport.hh>
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <std_srvs/SetBool.h>
 
-#include <gazebo/gazebo.hh>
-#include <gazebo/gazebo_client.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <gazebo/transport/transport.hh>
-
 using namespace gazebo;
-
-// information to record for the lights in the enviornment
-struct RecordInfo
-{
-  double duration;
-  double interval;
-  bool flashing;
-  common::Time lastUpdate;
-};
 
 /////////////////////////////////////////////////
 /// \brief A fixture class for testing the ROS flashlight plugin within Gazebo.
-class FlashLightTest : public testing::Test
+class FlashLihgtTest : public testing::Test
 {
   // Constructor.
-  public: FlashLightTest(): testing::Test()
+  public: FlashLihgtTest(): testing::Test()
   {
   }
 
   // Destructor.
-  public: virtual ~FlashLightTest()
+  public: virtual ~FlashLihgtTest()
   {
   }
+
+  // Callback for camera image
+  public: void CameraCb(ConstImageStampedPtr &_msg);
 
   // Callback for Responses about entity_info
   public: void ResponseCb(ConstResponsePtr &_msg);
 
-  /// \brief Initializer.
-  protected: void InitRec();
-
   /// \brief Send requests for entity information for a specified time.
   public: void SendRequests();
 
-  /// \brief A function to check if we got the assumed results.
-  /// \param[in] _updated Whether the light has been updated within its phase.
-  /// \param[in] _duration Expected duration time.
-  /// \param[in] _interval Expected interval time.
-  /// \param[in] _maxErr Maximum error which can be accepted in seconds.
-  protected: void CheckRec(
-    const std::vector<bool> &_updated,
-    const std::vector<double> &_duration,
-    const std::vector<double> &_interval,
-    const double &_maxErr);
-
   /// \brief Node for Gazebo transport.
   protected: gazebo::transport::NodePtr node;
+
+  /// \brief Subscriber to get the camera image.
+  protected: transport::SubscriberPtr cameraSub;
 
   /// \brief Publisher to request for entity_info
   protected: transport::PublisherPtr entityInfoPub;
@@ -89,17 +72,23 @@ class FlashLightTest : public testing::Test
   /// \brief ROS service client.
   protected: ros::ServiceClient client;
 
-  /// \brief A list of records about actual duration/interval.
-  private: struct RecordInfo flashLight[4];
-
   /// \brief True if a response is sent back.
-  private: bool responded;
+  protected: bool cameraCalled;
 
-  /// \brief The time when the records were initialized.
-  private: common::Time startTime;
+  /// \brief True if a visual object is flashing.
+  protected: bool visual_flashing;
 
   /// \brief Protect data from races.
-  private: std::mutex mutexData;
+  protected: std::mutex mutexCamera;
+
+  /// \brief True if a response is sent back.
+  protected: bool responded;
+
+  /// \brief True if a light object is flashing.
+  protected: bool light_flashing;
+
+  /// \brief Protect data from races.
+  protected: std::mutex mutexResponse;
 
   /// \brief A thread to send requests for entity_info
   protected: std::thread requester;
@@ -112,97 +101,49 @@ class FlashLightTest : public testing::Test
 };
 
 /////////////////////////////////////////////////
-void FlashLightTest::ResponseCb(ConstResponsePtr &_msg)
+void FlashLihgtTest::CameraCb(ConstImageStampedPtr &_msg)
 {
-  // Get the current time.
-  common::Time currentSimTime(ros::Time::now().toSec());
+  // Get the camera image
+  msgs::Image imageMsg = _msg->image();
+  common::Image image;
+  msgs::Set(image, imageMsg);
+  // Get the left top pixel.
+  ignition::math::Color color = image.Pixel(0, 0);
 
+  std::lock_guard<std::mutex> lk(this->mutexCamera);
+  this->visual_flashing = false;
+  if (color.R() > 0.9 && color.G() > 0.9 && color.B() > 0.9)
+    this->visual_flashing = true;
+
+  this->cameraCalled = true;
+}
+
+/////////////////////////////////////////////////
+void FlashLihgtTest::ResponseCb(ConstResponsePtr &_msg)
+{
   // Extract necessary information from the response message.
   msgs::Model modelMsg;
   modelMsg.ParseFromString(_msg->serialized_data());
 
-  int countLight = 0;
-  for (int ilink = 0; ilink < modelMsg.link_size(); ++ilink)
-  {
-    msgs::Link linkMsg = modelMsg.link(ilink);
-    for (int ilight = 0; ilight < linkMsg.light_size(); ++ilight)
-    {
-      msgs::Light lightMsg = linkMsg.light(ilight);
+  ASSERT_TRUE(modelMsg.link_size() > 0) << "The model must have a link.";
+  msgs::Link linkMsg = modelMsg.link(0);
+  ASSERT_TRUE(linkMsg.light_size() > 0) << "The link must have a light.";
+  msgs::Light lightMsg = linkMsg.light(0);
 
-      // Update to flash
-      {
-        std::lock_guard<std::mutex> lk(this->mutexData);
+  std::lock_guard<std::mutex> lk(this->mutexResponse);
 
-        // dim -> flash
-        if (!this->flashLight[countLight].flashing)
-        {
-          if (lightMsg.range() > 0)
-          {
-            this->flashLight[countLight].interval
-              = currentSimTime.Double()
-                - this->flashLight[countLight].lastUpdate.Double();
-            // Update the last update time
-            this->flashLight[countLight].lastUpdate = currentSimTime;
-            // Update the current flashing state.
-            this->flashLight[countLight].flashing = true;
-          }
-        }
-        // flash -> dim
-        else
-        {
-          if (lightMsg.range() == 0)
-          {
-            this->flashLight[countLight].duration
-              = currentSimTime.Double()
-                - this->flashLight[countLight].lastUpdate.Double();
-            // Update the last update time
-            this->flashLight[countLight].lastUpdate = currentSimTime;
-            // Update the current flashing state.
-            this->flashLight[countLight].flashing = false;
-          }
-        }
-      }
+  if (lightMsg.range() > 0)
+    this->light_flashing = true;
+  else
+    this->light_flashing = false;
 
-      ++countLight;
-    }
-  }
-  {
-    std::lock_guard<std::mutex> lk(this->mutexData);
-    this->responded = true;
-  }
+  this->responded = true;
 }
 
 /////////////////////////////////////////////////
-void FlashLightTest::InitRec()
+void FlashLihgtTest::SendRequests()
 {
-  common::Time currentSimTime(ros::Time::now().toSec());
-  {
-    std::lock_guard<std::mutex> lk(this->mutexData);
-    this->flashLight[0].duration = -1;
-    this->flashLight[1].duration = -1;
-    this->flashLight[2].duration = -1;
-    this->flashLight[3].duration = -1;
-    this->flashLight[0].interval = -1;
-    this->flashLight[1].interval = -1;
-    this->flashLight[2].interval = -1;
-    this->flashLight[3].interval = -1;
-    this->flashLight[0].flashing = false;
-    this->flashLight[1].flashing = false;
-    this->flashLight[2].flashing = false;
-    this->flashLight[3].flashing = false;
-    this->flashLight[0].lastUpdate = currentSimTime;
-    this->flashLight[1].lastUpdate = currentSimTime;
-    this->flashLight[2].lastUpdate = currentSimTime;
-    this->flashLight[3].lastUpdate = currentSimTime;
-    this->startTime = currentSimTime;
-    this->responded = false;
-  }
-}
-
-/////////////////////////////////////////////////
-void FlashLightTest::SendRequests()
-{
-  double timeToSleep = 0.1;
+  double timeToSleep = 0.2;
   while(true)
   {
     msgs::Request msg;
@@ -221,52 +162,22 @@ void FlashLightTest::SendRequests()
 }
 
 /////////////////////////////////////////////////
-void FlashLightTest::CheckRec(
-  const std::vector<bool> &_updated,
-  const std::vector<double> &_duration,
-  const std::vector<double> &_interval,
-  const double &_maxErr)
+TEST_F(FlashLihgtTest, switchOffAndOn)
 {
-  // Get necessary time information.
-  common::Time endTime(ros::Time::now().toSec());
-  double lastUp[4];
-  std::lock_guard<std::mutex> lk(this->mutexData);
-  ASSERT_TRUE(this->responded) << "The callback funciton was not called.";
-  lastUp[0] = this->flashLight[0].lastUpdate.Double();
-  lastUp[1] = this->flashLight[1].lastUpdate.Double();
-  lastUp[2] = this->flashLight[2].lastUpdate.Double();
-  lastUp[3] = this->flashLight[3].lastUpdate.Double();
+  // NOTE: this additional time is to make sure that a visual object to update
+  // has been created in the environment before publishing a message.
+  // Otherwise, a duplicate object will be created and the original one will
+  // never be updated.
+  // This problem is solved by the patch (Pull Request # 2983), which has
+  // been merged into gazebo7 as of July 16, 2018. This if satement should be
+  // removed once the patch is forwarded up to gazebo9.
+  ros::Duration(1.5).sleep();
 
-  // Check records of each light
-  // NOTE: Taking some errors caused by callback functions into consideration.
-  // NOTE: If the interval is 0, the callback is not called.
-  for (int i = 0; i < 4; ++i)
-  {
-    if (_updated[i] && _interval[i] > 0)
-    {
-      // The light is assumed to have been updated within its phase.
-      EXPECT_LE(endTime.Double() - lastUp[i],
-                std::max(_duration[i], _interval[i]) + _maxErr)
-      << "flashLight[" << i << "] is supposed to be updated.";
+  this->cameraCalled = false;
+  this->responded = false;
+  this->light_flashing = false;
+  this->visual_flashing = false;
 
-      // The light has been flashing by the assumed duration and interval.
-      EXPECT_NEAR(flashLight[i].duration, _duration[i], _maxErr)
-      << "flashLight[" << i << "] has a wrong duration time.";
-      EXPECT_NEAR(flashLight[i].interval, _interval[i], _maxErr)
-      << "flashLight[" << i << "] has a wrong interval time.";
-    }
-    else
-    {
-      // The light is assumed to have never been updated from the beginning.
-      EXPECT_LE(lastUp[i] - this->startTime.Double(), _maxErr)
-      << "flashLight[" << i << "] is supposed not to be updated.";
-    }
-  }
-}
-
-/////////////////////////////////////////////////
-TEST_F(FlashLightTest, switchOffAndOn)
-{
   // ROS spinning
   std::shared_ptr<ros::AsyncSpinner> async_ros_spin_;
   async_ros_spin_.reset(new ros::AsyncSpinner(0));
@@ -275,42 +186,53 @@ TEST_F(FlashLightTest, switchOffAndOn)
   // Initialize the transport node
   this->node = transport::NodePtr(new transport::Node());
   this->node->Init();
+  // Subcribe for camera.
+  this->cameraSub
+    = this->node->Subscribe("~/camera/link/camera/image",
+                            &FlashLihgtTest::CameraCb,
+                            dynamic_cast<FlashLihgtTest*>(this));
+
   this->entityInfoPub
     = this->node->Advertise<msgs::Request>("~/request");
   // Subcribe for entity_info.
   this->entityInfoSub
     = this->node->Subscribe("~/response",
-                            &FlashLightTest::ResponseCb,
-                            dynamic_cast<FlashLightTest*>(this));
+                            &FlashLihgtTest::ResponseCb,
+                            dynamic_cast<FlashLihgtTest*>(this));
 
   // Get a ROS service client.
   this->client
     = this->n.serviceClient<std_srvs::SetBool>("/light_model/light_switch");
-  ASSERT_TRUE(client.isValid());
-  ASSERT_TRUE(client.waitForExistence());
+  ASSERT_TRUE(this->client.isValid());
+  ASSERT_TRUE(this->client.waitForExistence());
 
   // Start the thread to repeatedly publishing a request.
   this->running = true;
-  this->requester = std::thread(&FlashLightTest::SendRequests,
-                                dynamic_cast<FlashLightTest*>(this));
+  this->requester = std::thread(&FlashLihgtTest::SendRequests,
+                                dynamic_cast<FlashLihgtTest*>(this));
 
   ros::Duration(0.5).sleep();
 
-  // Initialize the records.
-  this->InitRec();
-
-  ros::Duration(2.0).sleep();
-
-  // Check if the actual duration and interval are the supposed ones.
-  // NOTE: maximum error is set to 0.01 sec.
   {
-    std::vector<bool> updated = {true, true, false, false};
-    std::vector<double> duration = {0.4, 0.2, 2.0, 1.0};
-    std::vector<double> interval = {1.2, 0.2, 0.4, 0.0};
-    this->CheckRec(updated, duration, interval, 0.25);
+    std::lock_guard<std::mutex> lk(this->mutexResponse);
+    ASSERT_TRUE(this->responded);
+  }
+  {
+    std::lock_guard<std::mutex> lk(this->mutexCamera);
+    ASSERT_TRUE(this->cameraCalled);
   }
 
-  // Turn them off.
+  // Check if the light is flashing.
+  {
+    std::lock_guard<std::mutex> lk(this->mutexResponse);
+    EXPECT_TRUE(this->light_flashing) << "The light is not flashing.";
+  }
+  {
+    std::lock_guard<std::mutex> lk(this->mutexCamera);
+    EXPECT_TRUE(this->visual_flashing) << "The visual is not flashing.";
+  }
+
+  // Turn it off.
   {
     std_srvs::SetBool srv;
     srv.request.data = false;
@@ -318,25 +240,19 @@ TEST_F(FlashLightTest, switchOffAndOn)
     EXPECT_TRUE(success);
   }
 
-  // Allow some time for the plugin to stop blinking.
-  // After this, the callback function is not supposed to be called any more.
-  ros::Duration(0.2).sleep();
+  ros::Duration(0.5).sleep();
 
-  // Initialize the records.
-  this->InitRec();
-
-  ros::Duration(2.0).sleep();
-
-  // Check if all of them stopped to be updated.
-  // NOTE: maximum error is set to 0.01 sec.
+  // Check the results.
   {
-    std::vector<bool> updated = {false, false, false, false};
-    std::vector<double> duration = {-1, -1, -1, -1};
-    std::vector<double> interval = {-1, -1, -1, -1};
-    this->CheckRec(updated, duration, interval, 0.25);
+    std::lock_guard<std::mutex> lk(this->mutexResponse);
+    EXPECT_FALSE(this->light_flashing) << "The light was not turned off.";
+  }
+  {
+    std::lock_guard<std::mutex> lk(this->mutexCamera);
+    EXPECT_FALSE(this->visual_flashing) << "The visual was not turned off.";
   }
 
-  // Turn them on.
+  // Turn it on again.
   {
     std_srvs::SetBool srv;
     srv.request.data = true;
@@ -344,22 +260,16 @@ TEST_F(FlashLightTest, switchOffAndOn)
     EXPECT_TRUE(success);
   }
 
-  // Allow some time for the plugin to stop blinking.
-  // After this, the callback function is not supposed to be called any more.
-  ros::Duration(0.2).sleep();
+  ros::Duration(0.5).sleep();
 
-  // Initialize the records.
-  this->InitRec();
-
-  ros::Duration(3.0).sleep();
-
-  // Check if the duration and interval of lights were changed as expected.
-  // NOTE: maximum error is set to 0.01 sec.
+  // Check if the light is flashing.
   {
-    std::vector<bool> updated = {true, true, true, true};
-    std::vector<double> duration = {0.4, 0.2, 2.0, 1.0};
-    std::vector<double> interval = {1.2, 0.2, 0.4, 0.0};
-    this->CheckRec(updated, duration, interval, 0.25);
+    std::lock_guard<std::mutex> lk(this->mutexResponse);
+    EXPECT_TRUE(this->light_flashing) << "The light is not flashing.";
+  }
+  {
+    std::lock_guard<std::mutex> lk(this->mutexCamera);
+    EXPECT_TRUE(this->visual_flashing) << "The visual is not flashing.";
   }
 
   {
@@ -375,7 +285,7 @@ int main(int argc, char **argv)
   ::testing::InitGoogleTest(&argc, argv);
 
   // Start ROS
-  ros::init(argc,argv,"test_flashlight");
+  ros::init(argc,argv,"test_led");
 
   // Start Gazebo client
   gazebo::client::setup(argc, argv);
