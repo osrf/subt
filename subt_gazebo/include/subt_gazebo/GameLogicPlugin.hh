97275fa3cdac0777eef365c187478aef06b4170f
Carlos Aguero <devnull@localhost>
2018-08-09 15:58:18 -0700

    Compute score based on the observed pose of objects of interest.

--- subt_gazebo/include/subt_gazebo/GameLogicPlugin.hh
+++ subt_gazebo/include/subt_gazebo/GameLogicPlugin.hh
@@ -17,10 +17,18 @@
 #ifndef SUBT_GAZEBO_GAMELOGICPLUGIN_HH_
 #define SUBT_GAZEBO_GAMELOGICPLUGIN_HH_
 
+#include <geometry_msgs/PoseStamped.h>
+#include <ros/ros.h>
+#include <std_srvs/Trigger.h>
+#include <subt_msgs/ObjectOfInterest.h>
 #include <chrono>
+#include <map>
+#include <memory>
+#include <string>
 #include <gazebo/common/Event.hh>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/physics.hh>
+#include <ignition/math/Vector3.hh>
 #include <sdf/sdf.hh>
 
 namespace gazebo
@@ -29,7 +37,8 @@ namespace gazebo
   class GameLogicPlugin : public WorldPlugin
   {
     // Documentation inherited
-    public: virtual void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
+    public: virtual void Load(physics::WorldPtr _world,
+                              sdf::ElementPtr _sdf);
 
     /// \brief Callback for World Update events.
     private: void OnUpdate();
@@ -41,6 +50,23 @@ namespace gazebo
     /// \brief Callback triggered when the finish gate is crossed.
     /// \param[in] _msg The message containing if the gate was crossed or left.
     private: void OnFinish(const ignition::msgs::Boolean &_msg);
+
+    /// \brief Parse all the objects of interest.
+    /// \param[in] _sdf The SDF element containing the objects of interest.
+    private: void ParseObjectsOfInterest(sdf::ElementPtr _sdf);
+
+    /// \brief Callback executed to process a new object of interest request
+    /// sent by a team.
+    /// \param[in] _req The service request.
+    /// \param[out] _res The service response.
+    /// \return True when the service call succeed or false otherwise.
+    private: bool OnNewObjectOfInterest(
+      subt_msgs::ObjectOfInterest::Request &_req,
+      subt_msgs::ObjectOfInterest::Response &_res);
+
+    /// \brief ToDo.
+    private: double ScoreObjectOfInterest(
+      const geometry_msgs::PoseStamped &_pose);
 
     /// \brief World pointer.
     private: physics::WorldPtr world;
@@ -62,6 +88,18 @@ namespace gazebo
 
     /// \brief Finish time used for scoring.
     private: std::chrono::steady_clock::time_point finishTime;
+
+    /// \brief Store all objects of interest.
+    private: std::map<std::string, physics::ModelPtr> objectsOfInterest;
+
+    /// \brief The ROS node handler used for communications.
+    private: std::unique_ptr<ros::NodeHandle> rosnode;
+
+    /// \brief Service server for processing objects of interest.
+    private: ros::ServiceServer objectOfInterestSrv;
+
+    /// \brief Total score.
+    private: double totalScore = 0.0;
   };
 }
 #endif
