d3543c556b0445bbe2c9ee8007401ea9e483d028
Steve Peters <scpeters@openrobotics.org>
2018-10-18 17:58:04 -0700

    Store vector of weak_ptr to joints and increment elapsedTime when joint motion is detected

--- subt_gazebo/src/JointMotionTimerPlugin.cc
+++ subt_gazebo/src/JointMotionTimerPlugin.cc
@@ -16,9 +16,14 @@
 */
 
 #include <functional>
+#include <vector>
+#include <boost/weak_ptr.hpp>
 #include <subt_gazebo/JointMotionTimerPlugin.hh>
 #include <gazebo/common/Events.hh>
+#include <gazebo/physics/Joint.hh>
 #include <gazebo/physics/Model.hh>
+#include <gazebo/physics/PhysicsEngine.hh>
+#include <gazebo/physics/World.hh>
 
 namespace subt_gazebo
 {
@@ -26,8 +31,19 @@ GZ_REGISTER_MODEL_PLUGIN(JointMotionTimerPlugin)
 
 class JointMotionTimerPluginPrivate
 {
+  /// \brief Elapsed time.
+  public: double elapsedTime = 0.0;
+
+  /// \brief PhysicsEngine pointer for getting timestep.
+  public: gazebo::physics::PhysicsEnginePtr engine;
+
   /// \brief Pointer to the model that defines this plugin
   public: gazebo::physics::ModelPtr model;
+
+  using JointWeakPtr = boost::weak_ptr<gazebo::physics::Joint>;
+
+  /// \brief Joints to check for motion.
+  public: std::vector<JointWeakPtr> joints;
 
   /// \brief World update connection pointer.
   public: gazebo::event::ConnectionPtr updateConnection;
@@ -43,6 +59,19 @@ void JointMotionTimerPlugin::Load(gazebo::physics::ModelPtr _parent,
 {
   this->dataPtr->model = _parent;
 
+  auto world = _parent->GetWorld();
+  if (!world)
+  {
+    gzerr << "Unable to get world pointer." << std::endl;
+    return;
+  }
+  this->dataPtr->engine = world->Physics();
+  if (!this->dataPtr->engine)
+  {
+    gzerr << "Unable to get physics engine pointer." << std::endl;
+    return;
+  }
+
   this->dataPtr->updateConnection =
       gazebo::event::Events::ConnectWorldUpdateBegin(
           std::bind(&JointMotionTimerPlugin::OnUpdate, this));
@@ -50,5 +79,25 @@ void JointMotionTimerPlugin::Load(gazebo::physics::ModelPtr _parent,
 
 void JointMotionTimerPlugin::OnUpdate()
 {
+  double dt = this->dataPtr->engine->GetMaxStepSize();
+
+  bool motionDetected = false;
+  for (const auto &weakJoint : this->dataPtr->joints)
+  {
+    gazebo::physics::JointPtr joint = weakJoint.lock();
+    if (joint)
+    {
+      if (std::abs(joint->GetVelocity(0)) > 1e-2)
+      {
+        motionDetected = true;
+        break;
+      }
+    }
+  }
+
+  if (motionDetected)
+  {
+    this->dataPtr->elapsedTime += dt;
+  }
 }
 }
