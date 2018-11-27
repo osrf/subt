eab643a7f04a4ce6342918b1a9662cd76234c07b
Carlos Aguero <devnull@localhost>
2018-11-27 15:49:51 -0800

    Tunnel_practice_2 ready.

--- subt_gazebo/include/subt_gazebo/VisibilityTable.hh
+++ subt_gazebo/include/subt_gazebo/VisibilityTable.hh
@@ -40,10 +40,10 @@ namespace subt
     public: static const int32_t kMaxX = 500;
 
     /// \brief Min Y to sample.
-    public: static const int32_t kMinY = -150;
+    public: static const int32_t kMinY = -200;
 
     /// \brief Max Y to sample.
-    public: static const int32_t kMaxY = 150;
+    public: static const int32_t kMaxY = 200;
 
     /// \brief Min Z to sample.
     public: static const int32_t kMinZ = -50;
