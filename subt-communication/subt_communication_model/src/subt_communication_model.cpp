38ffd172260fb8c92ffacf4d99706e704b17f4f9
Jon Fink <jonathan.r.fink3.civ@mail.mil>
2019-01-08 11:37:07 -0500

    Fix communication model and add variance
    - Fix a bug in packet error computation
    - Draw from Gaussian distribution if rf function returns variance

--- subt-communication/subt_communication_model/src/subt_communication_model.cpp
+++ subt-communication/subt_communication_model/src/subt_communication_model.cpp
@@ -2,6 +2,7 @@
 #include <subt_communication_model/subt_communication_model.h>
 
 #include <math.h>
+#include <random>
 
 namespace subt
 {
@@ -13,10 +14,7 @@ inline double QPSKPowerToBER(double P, double N) { return erfc(sqrt(P / N)); }
 
 bool packetSuccess(double ber, uint64_t size)
 {
-  double packet_drop_prob = pow(ber, (double)size);
-  double rand_draw = (rand() % 1000) / 1000.0;
 
-  return rand_draw > packet_drop_prob;
 }
 
 
@@ -29,9 +27,18 @@ bool attempt_send(const radio_configuration& radio,
 
   // Get the received power based on TX power and position of each
   // node
-  auto rx_power = radio.pathloss_f(radio.default_tx_power,
-                                   tx_state,
-                                   rx_state);
+  auto rx_power_dist = radio.pathloss_f(radio.default_tx_power,
+                                        tx_state,
+                                        rx_state);
+
+  double rx_power = rx_power_dist.mean;
+  if(rx_power_dist.variance > 0.0) {
+
+    static std::default_random_engine rndEngine;
+    std::normal_distribution<> d{rx_power_dist.mean, sqrt(rx_power_dist.variance)};
+
+    rx_power = d(rndEngine);
+  }
 
   // Based on rx_power, noise value, and modulation, compute the bit
   // error rate (BER)
@@ -44,13 +51,16 @@ bool attempt_send(const radio_configuration& radio,
     ROS_WARN("Using unsupported modulation scheme!");
   }
 
+  double packet_drop_prob = 1.0 - exp(num_bytes*log(1-ber));
+
   ROS_DEBUG_STREAM("TX power (dBm): " << radio.default_tx_power << "\n" <<
                    "RX power (dBm): " << rx_power << "\n" <<
                    "BER: " << ber << "\n" <<
                    "# Bytes: " << num_bytes << "\n" <<
-                   "PER: " << pow(ber, (double)num_bytes));
+                   "PER: " << packet_drop_prob);
 
-  return packetSuccess(ber, num_bytes);
+  double rand_draw = (rand() % 1000) / 1000.0;
+  return rand_draw > packet_drop_prob;  
 }
 
 }
