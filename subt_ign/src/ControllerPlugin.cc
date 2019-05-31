b82643532b2194867dc195b393c44ad78d98770a
Carlos Aguero <devnull@localhost>
2019-05-31 16:44:41 -0700

    Log normal range working.

--- subt_ign/src/ControllerPlugin.cc
+++ subt_ign/src/ControllerPlugin.cc
@@ -40,7 +40,7 @@ bool ControllerPlugin::Load(const tinyxml2::XMLElement *_elem)
   }
 
   this->name = elem->GetText();
-  this->client.reset(new subt::CommsClientIgn(this->name, true));
+  this->client.reset(new subt::CommsClientIgn(this->name, false));
   this->client->Bind(&ControllerPlugin::OnMsg, this);
 
   ignmsg << this->name << " controller loaded" << std::endl;
