f844c6e5eb49fcc2526dde26db7a5eddb766e415
Nate Koenig <natekoenig@gmail.com>
2019-07-19 08:58:19 -0700

    headless

--- subt_ign/scripts/run_competition_x1x2.bash
+++ subt_ign/scripts/run_competition_x1x2.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ign launch -v 4 competition_no_ros.ign robotName1:=X1 robotConfig1:=X1_SENSOR_CONFIG1 robotName2:=X2 robotConfig2:=X2_SENSOR_CONFIG2
+ign launch -v 4 competition_no_ros.ign headless:=true robotName1:=X1 robotConfig1:=X1_SENSOR_CONFIG1 robotName2:=X2 robotConfig2:=X2_SENSOR_CONFIG2
