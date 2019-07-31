71c9fa5d6e2609f6a1a7fdad977f5dbf985ec250
Nate Koenig <natekoenig@gmail.com>
2019-07-31 13:18:59 -0700

    Tag docker images with commit hash

--- docker/build.bash
+++ docker/build.bash
@@ -37,10 +37,17 @@ fi
 user_id=$(id -u)
 image_name=$(basename $1)
 image_plus_tag=$image_name:$(date +%Y_%b_%d_%H%M)
+hg_id=$(hg id -i)
+
 
 shift
 
 docker build --rm -t $image_plus_tag --build-arg user_id=$user_id "$@" $DIR/$image_name
 docker tag $image_plus_tag $image_name:latest
+docker tag $image_plus_tag $image_name:$hg_id
 
-echo "Built $image_plus_tag and tagged as $image_name:latest"
+echo "------"
+echo "$hg_id"
+echo "------"
+
+echo "Built $image_plus_tag and tagged as $image_name:latest and $image_name:$hg_id"
