76e03fdb4860b1f1df0f43b2c03b89a47744157a
Addisu Z. Taddese <addisu@openrobotics.org>
2019-12-13 09:28:44 -0600

    Update to match existing sensor payloads

--- submitted_models/costar_husky_sensor_config_1/launch/spawner.rb
+++ submitted_models/costar_husky_sensor_config_1/launch/spawner.rb
@@ -1,10 +1,10 @@
-def spawner(_name, _modelURI, _worldName, _x, _y)
+def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw)
   <<-HEREDOC
   <plugin name="ignition::launch::GazeboFactory"
           filename="libignition-launch-gazebo-factory.so">
     <name>#{_name}</name>
     <allow_renaming>false</allow_renaming>
-    <pose>#{_x} #{_y} 0.2 0 0 0</pose>
+    <pose>#{_x} #{_y} #{_z + 0.2} #{_roll} #{_pitch} #{_yaw}</pose>
     <world>#{_worldName}</world>
     <is_performer>true</is_performer>
     <sdf version='1.6'>
@@ -45,15 +45,26 @@ def spawner(_name, _modelURI, _worldName, _x, _y)
         <power_load>6.6</power_load>
         <start_on_motion>true</start_on_motion>
       </plugin>
+      <!-- Gas Sensor plugin -->"
+      <plugin filename="libGasEmitterDetectorPlugin.so"
+        name="subt::GasDetector">
+        <topic>/model/#{_name}/gas_detected</topic>
+        <update_rate>10</update_rate>
+        <type>gas</type>
+      </plugin>
     </include>
     </sdf>
   </plugin>
+  HEREDOC
+end
+
+def rosExecutables(_name, _worldName)
+  <<-HEREDOC
   <executable name='robot_description'>
     <command>roslaunch --wait costar_husky_sensor_config_1 description.launch world_name:=#{_worldName} name:=#{_name}</command>
   </executable>
   <executable name='topics'>
     <command>roslaunch --wait costar_husky_sensor_config_1 vehicle_topics.launch world_name:=#{_worldName} name:=#{_name}</command>
   </executable>
-
   HEREDOC
 end
