e33bde4f235c8daa0764c2e1a5f4121097928b40
Nate Koenig <natekoenig@gmail.com>
2020-03-13 10:35:55 -0700

    update network log storage location

--- docker/cloudsim_sim/run_sim.bash
+++ docker/cloudsim_sim/run_sim.bash
@@ -3,8 +3,10 @@
 . /opt/ros/melodic/setup.bash
 . ~/subt_ws/install/setup.sh
 
+mkdir -p /tmp/ign/logs
+
 # Network traffic monitoring
-FILE=/tmp/ign/network_traffic.csv
+FILE=/tmp/ign/logs/network_traffic.csv
 echo "unix timestamp;iface_name;bytes_out/s;bytes_in/s;bytes_total/s;bytes_in;bytes_out;packets_out/s;packets_in/s;packets_total/s;packets_in;packets_out;errors_out/s;errors_in/s;errors_in;errors_out" > $FILE
 bwm-ng -o csv -c 0 -t 1000 -T rate -I eth0 >> $FILE &
 
