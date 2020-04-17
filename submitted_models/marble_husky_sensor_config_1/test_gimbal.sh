48d1847fe289d1918f5e3fc4c4751de728c94e7b
Neil Johnson <realdealneil@gmail.com>
2020-04-17 14:37:56 -0600

    Removed husky files from hd2 branch.  These should be the other branch, not here.

--- submitted_models/marble_husky_sensor_config_1/test_gimbal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### Move pan and tilt axes and set back near start position:
-rostopic pub -1 /X1/pan_tilt/tilt_rate_cmd_double std_msgs/Float64 -- 3.0
-rostopic pub -1 /X1/pan_tilt/tilt_rate_cmd_double std_msgs/Float64 -- -3.0
-rostopic pub -1 /X1/pan_tilt/tilt_rate_cmd_double std_msgs/Float64 -- 0.0
-rostopic pub -1 /X1/pan_tilt/pan_rate_cmd_double std_msgs/Float64 -- 3.0
-rostopic pub -1 /X1/pan_tilt/pan_rate_cmd_double std_msgs/Float64 -- -3.0
-rostopic pub -1 /X1/pan_tilt/pan_rate_cmd_double std_msgs/Float64 -- 0.0
