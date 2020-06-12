7b2f7e2169bc06368894bf552a74454e65131830
Ian Chen <ichen@osrfoundation.org>
2020-06-12 00:24:41 -0700

    add niosh segment 01 sections, dat and dot
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- docker/download_models.sh
+++ docker/download_models.sh
@@ -92,7 +92,17 @@ ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/R
 
 # Download niosh models
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Staging Area" -v 4
-ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 01" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 02" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 03" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 04" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 05" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 06" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 07" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 08" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 09" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 10" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 11" -v 4
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 02" -v 4
 
 # Download urban circuit tiles
