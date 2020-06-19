8514c5d1637829d1795cb44bb5c2a0fa0e244f05
Nate Koenig <nate@openrobotics.org>
2020-06-19 14:29:41 -0700

    Added model to download
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- docker/download_models.sh
+++ docker/download_models.sh
@@ -61,6 +61,7 @@ ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/M
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/CERBERUS_ANYMAL_B_SENSOR_CONFIG_1" -v 4
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/MARBLE_HUSKY_SENSOR_CONFIG_1" -v 4
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/MARBLE_HD2_SENSOR_CONFIG_1" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/CERBERUS_M100_SENSOR_CONFIG_1" -v 4
 
 # Download the tunnel circuit models
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/subt_tunnel_staging_area" -v 4
