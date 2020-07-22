135db86bdab2f1453bdadfa2c97012ce84421c9b
Nate Koenig <nate@openrobotics.org>
2020-07-22 14:26:27 -0700

    Adding in breadcrumb model
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- docker/download_models.sh
+++ docker/download_models.sh
@@ -70,6 +70,7 @@ ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/M
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/ROBOTIKA_KLOUBAK_SENSOR_CONFIG_2" -v 4
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/EXPLORER_R2_SENSOR_CONFIG_2" -v 4
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/ROBOTIKA_FREYJA_SENSOR_CONFIG_2" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/Breadcrumb Node" -v 4
 
 # Download the tunnel circuit models
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/subt_tunnel_staging_area" -v 4
