1c45c893500038dd38e1e9fa0ec02cb3ba99cc12
Martin Pecka <peckama2@fel.cvut.cz>
2020-08-03 08:25:09 +0200

    Added CTU_CRAS_NORLAB_ABSOLEM_SENSOR_CONFIG_1 and CTU_CRAS_NORLAB_ABSOLEM_SENSOR_CONFIG_2 (which only differ by breadcrumbs).

---
+++ submitted_models/ctu_cras_norlab_absolem_sensor_config_1/config/sdf.sh
@@ -0,0 +1,3 @@
+config="${config} rendering_target:=sdf flipper_inflation_ratio:=1.0 flipper_inward_enlargement:=0.0 
+  realsense_tf_from_urdf:=1 simulate_sick_lidar:=1 simulate_omnicam:=1 simulate_imu:=1
+  simulate_realsense_d435:=1"
