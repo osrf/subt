40f1186627e45ad30f27ed98f358de4c92d17b67
Ashton Larkin <ashton@openrobotics.org>
2020-08-07 11:11:50 -0400

    added timeout to flip detection that handles false positives
    Signed-off-by: Ashton Larkin <ashton@openrobotics.org>

--- subt_ign/src/GameLogicPlugin.cc
+++ subt_ign/src/GameLogicPlugin.cc
@@ -297,6 +297,11 @@ class subt::GameLogicPluginPrivate
   /// \brief A map of robot name and its previous pose
   public: std::map<std::string, ignition::math::Pose3d> robotPrevPose;
 
+  /// \brief A map of robot name to its flip information.
+  /// The value is a pair stating the sim time where the most recent flip started,
+  /// and if the robot is currently flipped.
+  public: std::map<std::string, std::pair<int64_t, bool>> robotFlipInfo;
+
   /// \brief Robot name with the max velocity
   public: std::pair<std::string, double> maxRobotVel = {"", 0};
 
@@ -1879,20 +1884,40 @@ void GameLogicPluginPrivate::CheckRobotFlip()
 {
   for (const auto &posePair : this->robotPrevPose)
   {
+    auto name = posePair.first;
+    auto pose = posePair.second;
+
+    if (this->robotFlipInfo.find(name) == this->robotFlipInfo.end())
+    {
+      this->robotFlipInfo[name] = {this->simTime.sec(), false};
+      continue;
+    }
+
     // Get cos(theta) between the world's z-axis and the robot's z-axis
     // If they are in opposite directions (cos(theta) close to -1), robot is flipped
-    ignition::math::Vector3d a = posePair.second.Rot() * ignition::math::Vector3d(0,0,1);
+    ignition::math::Vector3d a = pose.Rot() * ignition::math::Vector3d(0,0,1);
     auto cos_theta = a.Z();
-
-    if (std::abs(-1 - cos_theta) <= 0.1)
+    if (std::abs(-1 - cos_theta) <= 0.1 )
     {
-      std::ostringstream stream;
-      stream
-        << "- event:\n"
-        << "  type: flip\n"
-        << "  time_sec: " << this->simTime.sec() << "\n"
-        << "  robot:" << posePair.first << "\n";
-      this->LogEvent(stream.str());
+      // make sure the robot has been flipped for a few seconds before logging a flip
+      // (avoid false positives)
+      auto simElapsed = this->simTime.sec() - this->robotFlipInfo[name].first;
+      if (!this->robotFlipInfo[name].second && (simElapsed >= 3))
+      {
+        this->robotFlipInfo[name].second = true;
+
+        std::ostringstream stream;
+        stream
+          << "- event:\n"
+          << "  type: flip\n"
+          << "  time_sec: " << this->simTime.sec() << "\n"
+          << "  robot:" << name << "\n";
+        this->LogEvent(stream.str());
+      }
+    }
+    else
+    {
+      this->robotFlipInfo[name] = {this->simTime.sec(), false};
     }
   }
 }
