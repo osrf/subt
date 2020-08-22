b260faa033796088bd804a9b6a0158c318f4200c
Addisu Z. Taddese <addisu@openrobotics.org>
2020-08-22 02:26:19 -0500

    Add wheel slip plugin to vehicles
    The Wheel slip plugin dynamically adjusts the slip parameter of wheels
    in proportion to the speed of the vehicle. This is a better (i.e. more
    realistic) model of tires than the raw slip parameter in <surface>
    elements. When the vehicle is not moving, the plugin sets the slip
    parameter to 0, which prevents sliding.
    
    I have set the `<slip_compliance_lateral>` lateral parameter
    experimentally to maintain the current turning behavior of the vehicles.
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- submitted_models/robotika_x2_sensor_config_1/launch/spawner.rb
+++ submitted_models/robotika_x2_sensor_config_1/launch/spawner.rb
@@ -19,10 +19,10 @@ def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw)
   "      <wheel_separation>#{0.33559 * 1.23}</wheel_separation>\n"\
   "      <wheel_radius>0.098</wheel_radius>\n"\
   "      <topic>/model/#{_name}/cmd_vel_relay</topic>\n"\
-  "      <min_velocity>-2</min_velocity>
-  "      <max_velocity>2</max_velocity>
-  "      <min_acceleration>-6</min_acceleration>
-  "      <max_acceleration>6</max_acceleration>
+  "      <min_velocity>-2</min_velocity>\n"\
+  "      <max_velocity>2</max_velocity>\n"\
+  "      <min_acceleration>-6</min_acceleration>\n"\
+  "      <max_acceleration>6</max_acceleration>\n"\
   "    </plugin>\n"\
   "    <!-- Publish robot state information -->\n"\
   "    <plugin filename=\"libignition-gazebo-pose-publisher-system.so\"\n"\
@@ -57,6 +57,34 @@ def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw)
   "      <update_rate>10</update_rate>\n"\
   "      <type>gas</type>\n"\
   "    </plugin>\n"\
+  "      <!-- Wheel slip -->\n"\
+  "      <plugin filename=\"libignition-gazebo-wheel-slip-system.so\"\n"\
+  "        name=\"ignition::gazebo::systems::WheelSlip\">\n"\
+  "        <wheel link_name=\"front_left_wheel\">\n"\
+  "          <slip_compliance_lateral>0.35</slip_compliance_lateral>\n"\
+  "          <slip_compliance_longitudinal>0</slip_compliance_longitudinal>\n"\
+  "          <wheel_normal_force>45.20448</wheel_normal_force>\n"\
+  "          <wheel_radius>0.098</wheel_radius>\n"\
+  "        </wheel>\n"\
+  "        <wheel link_name=\"rear_left_wheel\">\n"\
+  "          <slip_compliance_lateral>0.35</slip_compliance_lateral>\n"\
+  "          <slip_compliance_longitudinal>0</slip_compliance_longitudinal>\n"\
+  "          <wheel_normal_force>45.20448</wheel_normal_force>\n"\
+  "          <wheel_radius>0.098</wheel_radius>\n"\
+  "        </wheel>\n"\
+  "        <wheel link_name=\"front_right_wheel\">\n"\
+  "          <slip_compliance_lateral>0.35</slip_compliance_lateral>\n"\
+  "          <slip_compliance_longitudinal>0</slip_compliance_longitudinal>\n"\
+  "          <wheel_normal_force>45.20448</wheel_normal_force>\n"\
+  "          <wheel_radius>0.098</wheel_radius>\n"\
+  "        </wheel>\n"\
+  "        <wheel link_name=\"rear_right_wheel\">\n"\
+  "          <slip_compliance_lateral>0.35</slip_compliance_lateral>\n"\
+  "          <slip_compliance_longitudinal>0</slip_compliance_longitudinal>\n"\
+  "          <wheel_normal_force>45.20448</wheel_normal_force>\n"\
+  "          <wheel_radius>0.098</wheel_radius>\n"\
+  "        </wheel>\n"\
+  "      </plugin>\n"\
   "  </include>\n"\
   "  </sdf>\n"\
   "</spawn>\n"\
