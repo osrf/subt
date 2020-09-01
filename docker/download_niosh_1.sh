1f707448bcd95df45317db9fbcb1251bbb00201b
Nate Koenig <nate@openrobotics.org>
2020-09-01 06:27:29 -0700

    Rename NIOSH tiles
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- docker/download_niosh_1.sh
+++ docker/download_niosh_1.sh
@@ -2,8 +2,8 @@
 
 # Download niosh models
 ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Staging Area" -v 4
-ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 01" -v 4
-ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 02" -v 4
-ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 03" -v 4
-ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 04" -v 4
-ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH Segment 01 Section 05" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH SR Course Section 01" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH SR Course Section 02" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH SR Course Section 03" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH SR Course Section 04" -v 4
+ign fuel download --url "https://fuel.ignitionrobotics.org/OpenRobotics/models/NIOSH SR Course Section 05" -v 4
