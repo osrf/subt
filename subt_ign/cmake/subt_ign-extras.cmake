1a07a0608ca64496f1104224a06cc75a9a046999
Nate Koenig <nate@openrobotics.org>
2020-11-09 12:22:34 -0800

    Update to use Ignition Dome
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- subt_ign/cmake/subt_ign-extras.cmake
+++ subt_ign/cmake/subt_ign-extras.cmake
@@ -1,4 +1,4 @@
-find_package(ignition-transport8 REQUIRED)
+find_package(ignition-transport9 REQUIRED)
 
-list(APPEND catkin_INCLUDE_DIRS ${ignition-transport8_INCLUDE_DIRS})
-list(APPEND catkin_LIBRARIES ${ignition-transport8_LIBRARIES} ${Protobuf_LIBRARIES})
+list(APPEND catkin_INCLUDE_DIRS ${ignition-transport9_INCLUDE_DIRS})
+list(APPEND catkin_LIBRARIES ${ignition-transport9_LIBRARIES} ${Protobuf_LIBRARIES})
