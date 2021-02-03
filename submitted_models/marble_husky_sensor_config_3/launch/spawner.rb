629004f2e2b18ba3645e94d70861a1f5638f3dd8
Ian Chen <ichen@osrfoundation.org>
2021-02-03 11:58:20 -0800

    reuse config_1 files, add config_4
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- submitted_models/marble_husky_sensor_config_3/launch/spawner.rb
+++ submitted_models/marble_husky_sensor_config_3/launch/spawner.rb
@@ -95,10 +95,10 @@ end
 def rosExecutables(_name, _worldName)
   <<-HEREDOC
   <executable name='robot_description'>
-    <command>roslaunch --wait marble_husky_sensor_config_3 description.launch world_name:=#{_worldName} name:=#{_name}</command>
+    <command>roslaunch --wait marble_husky_sensor_config_1 description.launch world_name:=#{_worldName} name:=#{_name}</command>
   </executable>
   <executable name='topics'>
-    <command>roslaunch --wait marble_husky_sensor_config_3 vehicle_topics.launch world_name:=#{_worldName} name:=#{_name}</command>
+    <command>roslaunch --wait marble_husky_sensor_config_1 vehicle_topics.launch world_name:=#{_worldName} name:=#{_name} thermal:=1</command>
   </executable>
   HEREDOC
 end
