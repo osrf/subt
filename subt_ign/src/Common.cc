aff0538ccccc525a007cdc0b15e58ae30cc831af
Nate Koenig <nate@openrobotics.org>
2021-02-04 21:25:31 -0800

    remove debug
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- subt_ign/src/Common.cc
+++ subt_ign/src/Common.cc
@@ -93,13 +93,11 @@ bool StringFromArtifact(const ArtifactType &_type,
 bool FullWorldPath(const std::string &_worldName,
                    std::string &_worldPath)
 {
-  std::cerr << "1\n";
   _worldPath = "";
   if (_worldName.empty())
   {
     return false;
   }
-  std::cerr << "2\n";
 
   std::string worldsDirectory = SUBT_INSTALL_WORLD_DIR;
 
@@ -108,7 +106,6 @@ bool FullWorldPath(const std::string &_worldName,
   const std::string cavePrefix = "cave_circuit_";
   if (0 == _worldName.compare(0, tunnelPrefix.size(), tunnelPrefix))
   {
-  std::cerr << "3\n";
     std::string suffix = _worldName.substr(tunnelPrefix.size());
     // don't use a subfolder for practice worlds
     if (0 != suffix.compare(0, 9, "practice_"))
@@ -119,7 +116,6 @@ bool FullWorldPath(const std::string &_worldName,
   }
   else if (_worldName.find(urbanPrefix) != std::string::npos)
   {
-  std::cerr << "4\n";
     std::string suffix = _worldName.substr(urbanPrefix.size());
     // don't use a subfolder for practice worlds
     if (0 != suffix.compare(0, 9, "practice_"))
@@ -130,7 +126,6 @@ bool FullWorldPath(const std::string &_worldName,
   }
   else if (_worldName.find(cavePrefix) != std::string::npos)
   {
-  std::cerr << "5\n";
     std::string suffix = _worldName.substr(cavePrefix.size());
     // don't use a subfolder for practice worlds
     if (0 != suffix.compare(0, 9, "practice_"))
@@ -143,7 +138,6 @@ bool FullWorldPath(const std::string &_worldName,
            _worldName.find("_qual") == std::string::npos &&
            _worldName.find("_stix") == std::string::npos)
   {
-  std::cerr << "6[" << _worldName << "]\n";
     return false;
   }
 
