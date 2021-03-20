85ed8a3ec517866abf0c93c94fab1127c1ee0663
Martin Pecka <peckama2@fel.cvut.cz>
2021-03-20 16:06:05 +0100

    Added CTU_CRAS_NORLAB_MARV_SENSOR_CONFIG_1 (and other sensor configs).

---
+++ submitted_models/ctu_cras_norlab_marv_sensor_config_1/launch/spawner.rb
@@ -0,0 +1,9 @@
+require_relative 'common.rb'
+
+def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw, _additionalSDF='')
+  _spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw, _additionalSDF)
+end
+
+def rosExecutables(_name, _worldName)
+  _rosExecutables(_name, _worldName)
+end
