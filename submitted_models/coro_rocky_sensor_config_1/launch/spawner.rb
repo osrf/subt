c6f1deaeaaa672d863b98f738ebdbd1382e14b38
knoedler <knoedler@dslextreme.com>
2021-04-21 13:09:55 -0700

    Model Submission for Coordinated Robotics Rocky Sensor Configs 1, 2 and 3 (#862)
    * Coordinated Robotics Rocky
    
    * Updates based on reviews of other robots
    
    * Remove wheel slip plugin from Ackermann type steering vehicle
    
    * Fixed camera name, changed color to floats
    
    * Update cameras to match precedent
    
    Co-authored-by: Kevin <ak619@lafn.org>

---
+++ submitted_models/coro_rocky_sensor_config_1/launch/spawner.rb
@@ -0,0 +1,81 @@
+def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw)
+  <<-HEREDOC
+  <spawn name='#{_name}'>
+    <name>#{_name}</name>
+    <allow_renaming>false</allow_renaming>
+    <pose>#{_x} #{_y} #{_z + 0.1} #{_roll} #{_pitch} #{_yaw}</pose>
+    <world>#{_worldName}</world>
+    <is_performer>true</is_performer>
+    <sdf version='1.6'>
+    <include>
+      <name>#{_name}</name>
+      <uri>#{_modelURI}</uri>
+      <!-- Ackermann Steering -->
+      <plugin filename="libackermannrocky.so"
+              name="ignition::gazebo::systems::AckermannSteering">
+        <left_joint>front_left_wheel_joint</left_joint>
+        <left_joint>rear_left_wheel_joint</left_joint>
+        <right_joint>front_right_wheel_joint</right_joint>
+        <right_joint>rear_right_wheel_joint</right_joint>
+        <left_steering_joint>front_left_wheel_steering_joint</left_steering_joint>
+        <right_steering_joint>front_right_wheel_steering_joint</right_steering_joint>
+        <wheel_separation>0.285</wheel_separation>
+        <kingpin_width>0.22</kingpin_width>
+        <steering_limit>0.266</steering_limit>
+        <wheel_base>0.42</wheel_base>
+        <wheel_radius>0.075</wheel_radius>
+        <topic>/model/#{_name}/cmd_vel_relay</topic>
+        <min_velocity>-3.0</min_velocity>
+        <max_velocity>3.0</max_velocity>
+        <min_acceleration>-4</min_acceleration>
+        <max_acceleration>4</max_acceleration>
+      </plugin>
+      <!-- Publish robot state information -->
+      <plugin filename="libignition-gazebo-pose-publisher-system.so"
+        name="ignition::gazebo::systems::PosePublisher">
+        <publish_link_pose>true</publish_link_pose>
+        <publish_sensor_pose>true</publish_sensor_pose>
+        <publish_collision_pose>false</publish_collision_pose>
+        <publish_visual_pose>false</publish_visual_pose>
+        <publish_nested_model_pose>#{$enableGroundTruth}</publish_nested_model_pose>
+        <use_pose_vector_msg>true</use_pose_vector_msg>
+        <static_publisher>true</static_publisher>
+        <static_update_frequency>1</static_update_frequency>
+      </plugin>
+      <!-- Battery plugin -->
+      <plugin filename="libignition-gazebo-linearbatteryplugin-system.so"
+        name="ignition::gazebo::systems::LinearBatteryPlugin">
+        <battery_name>linear_battery</battery_name>
+        <voltage>12.694</voltage>
+        <open_circuit_voltage_constant_coef>12.694</open_circuit_voltage_constant_coef>
+        <open_circuit_voltage_linear_coef>-3.1424</open_circuit_voltage_linear_coef>
+        <initial_charge>78.4</initial_charge>
+        <capacity>78.4</capacity>
+        <resistance>0.061523</resistance>
+        <smooth_current_tau>1.9499</smooth_current_tau>
+        <power_load>3.4</power_load>
+        <start_on_motion>true</start_on_motion>
+      </plugin>
+      <!-- Gas Sensor plugin -->"
+      <plugin filename="libGasEmitterDetectorPlugin.so"
+        name="subt::GasDetector">
+        <topic>/model/#{_name}/gas_detected</topic>
+        <update_rate>10</update_rate>
+        <type>gas</type>
+      </plugin>
+    </include>
+    </sdf>
+  </spawn>
+  HEREDOC
+end
+
+def rosExecutables(_name, _worldName)
+  <<-HEREDOC
+  <executable name='robot_description'>
+    <command>roslaunch --wait coro_rocky_sensor_config_1 description.launch world_name:=#{_worldName} name:=#{_name}</command>
+  </executable>
+  <executable name='topics'>
+    <command>roslaunch --wait coro_rocky_sensor_config_1 vehicle_topics.launch world_name:=#{_worldName} name:=#{_name}</command>
+  </executable>
+  HEREDOC
+end
