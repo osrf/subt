d0fb469375e551efbaa5ff4e817dbe37b96f8b1b
Bryanna Yeh <heyyeh12@users.noreply.github.com>
2021-05-25 13:51:17 -0400

    updated specifications, bridged topics, default sensors

--- submitted_models/jhuapl_edge540xl_sensor_config_1/launch/spawner.rb
+++ submitted_models/jhuapl_edge540xl_sensor_config_1/launch/spawner.rb
@@ -1,4 +1,6 @@
-def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw, _plantURDF='urdf/Edge540XLPlant.urdf')
+def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw)
+  
+  _plantURDF = File.join(File.expand_path("../urdf", File.dirname(__FILE__)), "Edge540XLPlant.urdf")
   <<-HEREDOC
   <spawn name='#{_name}'>
     <name>#{_name}</name>
@@ -12,10 +14,12 @@ def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw, _plan
       <uri>#{_modelURI}</uri>
 
       <!-- Simulate plane dynamics -->
+    
       <plugin filename="libjhuapl_uav_dynamics.so"
         name="jhuapl::Edge540xlDynamics">
         <plant_urdf_file>#{_plantURDF}</plant_urdf_file>
         <model_name>#{_name}</model_name>
+        <topic>/model/#{_name}/input</topic>
         <link_name>fuselage</link_name>
         <min_x>-1000</min_x>
         <max_x>1000</max_x>
@@ -25,20 +29,57 @@ def spawner(_name, _modelURI, _worldName, _x, _y, _z, _roll, _pitch, _yaw, _plan
         <max_z>1000</max_z>
       </plugin>
 
+      <!-- Publish robot state information -->
+      <plugin filename="libignition-gazebo-pose-publisher-system.so"
+        name="ignition::gazebo::systems::PosePublisher">
+        <publish_link_pose>true</publish_link_pose>
+        <publish_sensor_pose>true</publish_sensor_pose>
+        <publish_collision_pose>false</publish_collision_pose>
+        <publish_visual_pose>false</publish_visual_pose>
+        <publish_nested_model_pose>#{$enableGroundTruth}</publish_nested_model_pose>
+        <use_pose_vector_msg>true</use_pose_vector_msg>
+        <static_publisher>true</static_publisher>
+        <static_update_frequency>1</static_update_frequency>
+      </plugin>
+
       <!-- Battery plugin -->
       <plugin filename="libignition-gazebo-linearbatteryplugin-system.so"
         name="ignition::gazebo::systems::LinearBatteryPlugin">
         <battery_name>linear_battery</battery_name>
-        <voltage>12.4</voltage>
-        <open_circuit_voltage_constant_coef>12.4</open_circuit_voltage_constant_coef>
+        <voltage>12.6</voltage>
+        <open_circuit_voltage_constant_coef>12.6</open_circuit_voltage_constant_coef>
         <open_circuit_voltage_linear_coef>-3.1424</open_circuit_voltage_linear_coef>
-        <initial_charge>12.3</initial_charge>
-        <capacity>12.3</capacity>
+        <initial_charge>2.2</initial_charge>
+        <capacity>2.2</capacity>
         <resistance>0.061523</resistance>
         <smooth_current_tau>1.9499</smooth_current_tau>
-        <power_load>5.214</power_load>
+        <power_load>2</power_load>
         <start_on_motion>true</start_on_motion>
       </plugin>
+
+      <!-- Joint state publisher -->
+      <plugin filename="libignition-gazebo-joint-state-publisher-system.so"
+        name="ignition::gazebo::systems::JointStatePublisher">
+      </plugin>
+
+      <!-- Gas Sensor plugin -->
+      <plugin filename="libGasEmitterDetectorPlugin.so"
+        name="subt::GasDetector">
+        <topic>/model/#{_name}/gas_detected</topic>
+        <update_rate>10</update_rate>
+        <type>gas</type>
+      </plugin>
+
+      <sensor name="air_pressure" type="air_pressure">
+        <always_on>1</always_on>
+        <update_rate>20</update_rate>
+        <air_pressure>
+            <reference_altitude>0</reference_altitude>
+            <noise type="gaussian">
+                <mean>0.00000008</mean>
+            </noise>
+        </air_pressure>
+      </sensor>
 
     </include>
     </sdf>
--- submitted_models/jhuapl_edge540xl_sensor_config_1/specifications.md
+++ submitted_models/jhuapl_edge540xl_sensor_config_1/specifications.md
@@ -12,9 +12,9 @@ The vehicle can be launched with the following command:
 ign launch -v 4 competition.ign robotName1:=edge540xl robotConfig1:=JHUAPL_EDGE540XL_SENSOR_CONFIG_1  circuit:=cave worldName:=simple_cave_01 localModel:=true
 ```
 
-At this point you can control the vehicle model with a actuator command, for example:
+At this point you can control the vehicle model with an actuator command, for example:
 ```
-rostopic pub -r10 /edge540xl/input mav_msgs/Actuators "angular_velocities: [0.3, 0.3, 0.3, 15.0]"
+rostopic pub -r 10 /edge540xl/input mav_msgs/Actuators "angular_velocities: [0.0, 0.1, 0.0, 15.0]"
 ```
 This commands the angular velocities of the aileron, elevator, and rudder, with thrust at the end.
 
@@ -22,21 +22,33 @@ This commands the angular velocities of the aileron, elevator, and rudder, with 
 The software included in this package is released under a BSD 3-Clause license.
 
 ### Cost and Scale
-The Edge540XL is a research platform and its cost is not publicly available at the moment. It weighs approximately 1.135 kg.
+The Edge540XL is a research platform and its cost is not publicly available at the moment. It weighs approximately 1.25 kg.
 
 ### Sensors
 This Edge540XL with sensor configuration 1 includes the following sensors.
 
 * Depth Camera - Intel Realsense D435, modeled by `rgbd_camera` plugin
+* IMU
+* Gas sensor
+* Barometer
+* Magnetometer 
 
 ### Control
 A high-level controller that takes in a goal Pose will be released in the future.
 
 ### Motion Characteristics
 
+* _x_ linear velocity range from 3.0 m/s to 15 m/s
+* _x_ linear acceleration range from -30.0 m/s<sup>2</sup> to 13 m/s<sup>2</sup>
+* control surface velocity range from -10 rad/s to 10 rad/s
+* thrust range from 0 N to 16 N
 
 ### Endurance Characteristics
 
+* battery capacity: 2.2Ah
+* voltage fully charged: 12.6V
+* maximum draw: 500W
+* average draw: ~200W
 
 ### Diversions from Physical Hardware of Edge540XL
 
