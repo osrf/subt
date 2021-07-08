80a2e7fb088dd1eb4ffa5952a2b6e52750bc956e
Ian Chen <ichen@osrfoundation.org>
2021-07-08 15:37:54 -0700

    revert ke factor, update ke height
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- subt_ign/include/subt_ign/RobotPlatformTypes.hh
+++ subt_ign/include/subt_ign/RobotPlatformTypes.hh
@@ -20,12 +20,12 @@
 /// \brief List of robot platform types and kinetic energy threshold factor. This is used to count unique robot types and determine crashes.
 const std::map<std::string, double> robotPlatformTypes = {
   {"ABSOLEM", 1},
-  {"ALLIE", 0.7},
+  {"ALLIE", 1},
   {"ANYMAL_B", 1},
   {"ANYMAL_C", 1},
   {"CRYSTAL", 8}, // UAV, no prop guards
   {"DS1", 6}, // UAV, prop guards
-  {"DTR", 0.7},
+  {"DTR", 1},
   {"FREYJA", 1},
   {"GAGARIN", 1}, // UAV, collision tolerant
   {"HD2", 1},
