916d05a66779a6ce974aba20df09cf4140dda740
Nate Koenig <nate@openrobotics.org>
2021-11-11 13:11:07 -0800

    Added scripts
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

---
+++ subt_ign/record_playback_events.bash
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# The playback event recorder creates videos for events that are stored
+# in the events.yml log file in the state log directory. The script launches
+# ign gazebo in playback mode and creates videos using the gui camera video
+# recording feature. For each event, it seeks playback to some time before the
+# event, moves the camera to the desired location, and starts recording until
+# some time after the event.
+
+# Start clean
+pkill -f "path_tracer"
+pkill -f "ign gazebo"
+docker kill $(docker ps -q)
+pkill -f "ign gazebo"
+
+echo "==================================="
+echo "Staring Playback Event Recorder"
+echo "==================================="
+
+if [ -z "$1" ]; then
+  echo "Usage: bash ./record_playback_events.bash [path_to_log_dir] [path_to_ouput_directory]"
+  exit 0
+fi
+
+logDirPath=$1
+videoDirPath=$2
+
+export SUBT_EVENT_ID=
+if [ $# -eq 3 ]; then
+  export SUBT_EVENT_ID=$3
+fi
+
+if [ ! -d "$logDirPath" ]; then
+  echo "Directory does not exist: $logDirPath"
+  exit 0
+fi
+
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+tmpDir="$scriptDir/tmp_record"
+
+echo "Creating tmp dir for recording: $tmpDir"
+
+if [ -d "$tmpDir" ]; then
+ rm -fr $tmpDir
+fi
+
+ln -s $logDirPath $tmpDir
+
+echo "Starting log playback and video recording"
+echo "Starting $logDirPath" >> record_status.log
+
+export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$LD_LIBRARY_PATH:$IGN_LAUNCH_PLUGIN_PATH
+sdfName="playback_event_recorder"
+IGN_TRANSPORT_TOPIC_STATISTICS=1 ign gazebo -s -v 4 $sdfName.sdf
+
+echo "Video recording ended. Shutting down playback"
+
+pgrep -f $sdfName | xargs kill -9 &> /dev/null
+
+echo "Moving mp4 videos to dir: $videoDirPath"
+mv *.mp4 $videoDirPath
+
+# remove tmp dir
+if [ -d "$tmpDir" ]; then
+ rm -fr $tmpDir
+fi
+echo "Done"
